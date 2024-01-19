765e5a56856d5271c64eb39139901671bf8fd275
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-01-19 11:41:40 +0700

    Cập nhật code
    sửa mới và thêm dòng

--- HelloWorld/src/tiil/edu/XinChao.java
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -13,8 +13,8 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		//
-		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, và đang thử nghiệm quản lý mã nguồn với GIT");
-
+		System.out.println("TÔI LẦN ĐẦU HỌC JaVa, và đang thử nghiệm quản lý mã nguồn với GIT");
+		System.out.print("tôi thêm 1 dòng mới, sửa lại chữ Java ở trên");
 	}
 
 }
