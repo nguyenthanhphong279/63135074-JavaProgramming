0c3d6f42c817b6b71d1e54fd259f81433da4900e
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-01-25 15:30:20 +0700

    Làm Bài 4
    Làm bài 4 lab 1

--- Lab1/src/till/edu/Lab1Bai4.java
+++ Lab1/src/till/edu/Lab1Bai4.java
@@ -3,6 +3,8 @@
  */
 package till.edu;
 
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,6 +15,16 @@ public class Lab1Bai4 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		//Nhập các hệ số a,b,c của phương trình bậc 2
+		System.out.print("Nhập a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Nhập b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Nhập c: ");
+        double c = scanner.nextDouble();
+        //Tính delta
+        double delta = Math.pow(b, 2) - 4 * a * c;
 
 	}
 
