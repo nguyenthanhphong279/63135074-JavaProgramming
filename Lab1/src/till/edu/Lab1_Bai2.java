c44504c344f43308f6e59c745cbee682ad103812
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-01-25 13:58:02 +0700

    Lab1_Bai2
    Làm bài 2 của lab 1

---
+++ Lab1/src/till/edu/Lab1_Bai2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package till.edu;
+
+/**
+ * 
+ */
+public class Lab1_Bai2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+
+	}
+
+}
