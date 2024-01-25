ab2cdbe71382f7f5131c4bd17e04d1925d169661
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-01-25 18:58:16 +0700

    Bài 2 Lab 2
    Lưu

---
+++ Lab2/src/tiil/edu/Lab2Bai2.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+import java.util.Scanner;
+
+public class Lab2Bai2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		//Nhap cac he so a,b,c
+		System.out.print("Nhập hệ số a: ");
+		double a = scanner.nextDouble();
+		
+		System.out.print("Nhập hệ số b: ");
+		double b = scanner.nextDouble();
+		
+		System.out.print("Nhập hệ số c: ");
+		double c = scanner.nextDouble();
+		
+
+	}
+
+}
