54cf3f3a8f2c1e3b84dae0ae1c3ceae96e55d7d2
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-01-25 16:35:40 +0700

    Bài 1 Lab 2
    Làm Bài 1 Lab 2

--- Lab2/src/tiil/edu/Lab2Bai1.java
+++ Lab2/src/tiil/edu/Lab2Bai1.java
@@ -3,17 +3,29 @@
  */
 package tiil.edu;
 
-/**
- * 
- */
+import java.util.Scanner;
+
 public class Lab2Bai1 {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		//Nhập hệ số a,b vào phương trình bậc nhất
+		System.out.print("Nhập hệ số của a: ");
+		double a = scanner.nextDouble();
+		System.out.print("Nhập hệ số của b: ");
+		double b = scanner.nextDouble();
+		//Kiểm tra a,b để giải phương trình
+		if (a == 0) {
+			if (b == 0) {
+				System.out.println("Phương trình có vô số nghiệm");
+			}
+			else {
+				System.out.println("Phương trình vô nghiệm");
+			}
+		}
+		else {
+			
+		}
+		
 	}
 
 }
