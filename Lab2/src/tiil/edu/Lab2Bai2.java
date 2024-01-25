6b8bf3d9706ab9717ff0d7f41c425b00b74b3d3c
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-01-25 20:14:37 +0700

    Bài 2 Lab 2
    Chỉnh sửa Bài 2 của lab 2

--- Lab2/src/tiil/edu/Lab2Bai2.java
+++ Lab2/src/tiil/edu/Lab2Bai2.java
@@ -21,26 +21,33 @@ public class Lab2Bai2 {
 		// Xét a
 		if (a == 0){
 			if (b == 0){
-				System.out.println("Phương trình có vô số nghiệm.");
-			}else {
-				System.out.println("Phương trình vô nghiệm.");
+				if (c == 0) {
+					System.out.println("Phương trình có vô số nghiệm.");
+				}
+				else {
+					System.out.println("Phương trình vô nghiệm.");
+				}
+			}
+			else {
+				double x = -c / b;
+				System.out.println("Nghiệm của phương trình là: " + x);
 			}
 		}
-		
-		//Tính delta
-		double delta = b * b - 4 * a * c;
-		
-		if (delta < 0) {
-			System.out.println("Phương trình vô nghiệm.");
-		}else if (delta == 0) {
-			double x = -b / (2 * a);
-			System.out.println("Phương trình có nghiệm kép: " + x);
-		}else {
-			double x1 = (-b + Math.sqrt(delta)) / (2 * a);
-			double x2 = (-b - Math.sqrt(delta)) / (2 * a);
-			System.out.println("Phương trình có hai nghiệm phân biệt: x1 = " + x1 + ", x2 = " + x2);
+		else {
+			//Tính delta
+			double delta = b * b - 4 * a * c;
+			
+			if (delta < 0) {
+				System.out.println("Phương trình vô nghiệm.");
+			}else if (delta == 0) {
+				double x = -b / (2 * a);
+				System.out.println("Phương trình có nghiệm kép: " + x);
+			}else {
+				double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+				double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+				System.out.println("Phương trình có hai nghiệm phân biệt: x1 = " + x1 + ", x2 = " + x2);
+			}
 		}
-
+		scanner.close();
 	}
-
 }
