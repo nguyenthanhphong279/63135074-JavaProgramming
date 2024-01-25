e1eee468c61b299adebc34130f4353d3a430de13
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-01-25 15:16:51 +0700

    Lab1Bai4
    Làm Lab1 Bài 4

---
+++ Lab1/src/till/edu/Lab1Bai4.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package till.edu;
+
+/**
+ * 
+ */
+public class Lab1Bai4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
