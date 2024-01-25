04e81573b42971bf461d2a00c9cddfd2f7a3398e
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-01-25 19:16:40 +0700

    Update Lab2Bai2.java

--- Lab2/src/tiil/edu/Lab2Bai2.java
+++ Lab2/src/tiil/edu/Lab2Bai2.java
@@ -18,6 +18,19 @@ public class Lab2Bai2 {
 		System.out.print("Nhập hệ số c: ");
 		double c = scanner.nextDouble();
 		
+		//Tính delta
+		double delta = b * b - 4 * a * c;
+		
+		if (delta < 0) {
+			System.out.println("Phương trình vô nghiệm.");
+		}else if (delta == 0) {
+			double x = -b / (2 * a);
+			System.out.println("Phương trình có nghiệm kép: " + x);
+		}else {
+			double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+			double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+			System.out.println("Phương trình có hai nghiệm phân biệt: x1 = " + x1 + ", x2 = " + x2);
+		}
 
 	}
 
