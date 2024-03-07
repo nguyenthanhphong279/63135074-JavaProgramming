a2a68c9ddffe08f233a885a09c6af8d37cc8fbee
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-03-07 18:26:17 +0700

    Bắt đầu làm lab5 bài 3

---
+++ Lab5/src/tiil/edu/Lab5Bai3.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Lab5Bai3 {
+	class SanPham{
+		String ten;
+		Double gia;
+		
+		public SanPham(String ten, Double gia) {
+			this.ten = ten;
+			this.gia = gia;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<SanPham> ds = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
+
+		
+	}
+
+}
